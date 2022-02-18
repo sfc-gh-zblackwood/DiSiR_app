cec95d170c583049b1c23c19116f69175f57aafc
akurlovs <34013941+akurlovs@users.noreply.github.com>
2022-02-18 02:31:59 +0100

    overhaul how the data is loaded

--- disir_app.py
+++ disir_app.py
@@ -17,16 +17,16 @@ st.title('DiSiR')
 
 col1, col2 = st.columns(2)
 
-uploaded_mtx = st.file_uploader("Provide path to matrix.mtx", key="mtx")
+uploaded_mtx = st.file_uploader("Upload matrix.mtx", key="mtx")
 
 uploaded_genes = col1.file_uploader("Upload genes.txt")
 uploaded_metadata = col1.file_uploader("Upload metadata (csv or json)")
 select_track = col1.text_input('Metadata column with cell annotatation', value='CellStates')
 
-threshold_fractions = col2.number_input('min fraction of cells in a cell type that expresses the gene', min_value = 0.0, max_value=1.0, value=0.0, step=0.1)
-threshold_expressions = col2.number_input('threshold on scaled (max-normalized) average expression of each ligand or receptor within a cell type', min_value = 0.0, value=0.0, step=0.1)
+threshold_fractions = col2.number_input('min fraction of cells in a cell type that expresses the gene', min_value = 0, max_value=1, value=0)
+threshold_expressions = col2.number_input('threshold on scaled (max-normalized) average expression of each ligand or receptor within a cell type', min_value = 0, value=0)
 threshold_number = col2.number_input('min number of cells in a cell type that expresses the gene', min_value = 0, value=0)
-iteration = col2.number_input('number of permutations', min_value=1, max_value=1000000,value=500, step=100)
+iteration = col2.number_input('number of permutations', min_value=1, max_value=1000000,value=500)
 
 EXCLUDE_ITEMS = ['Unclassified']
 THRESHOLD = 0.05 # p-value, essentially
@@ -38,26 +38,17 @@ if uploaded_mtx and uploaded_genes and uploaded_metadata:
     ligand_names = st.text_input('Please choose the ligands of interest in order, separated by commas').replace(' ', '').split(',')
     receptor_names = st.text_input('Please choose the receptor of interest in order, separated by commas').replace(' ', '').split(',')
     
-    if 'metadata' not in st.session_state and 'scRNA' not in st.session_state:
-        data_load_state = st.text('If you are loading your matrix for the first time, it might take a while...')
-        scRNA_array, metadata = ds.load_data(uploaded_mtx, uploaded_metadata)
-        st.session_state['metadata'] = metadata
-        st.session_state['scRNA'] = scRNA_array
-        data_load_state.text('Done!')
-    else:
-        metadata = st.session_state['metadata']
-        scRNA_array = st.session_state['scRNA']
+    metadata = ds.load_metadata(uploaded_metadata)
     
-    categories = sorted(metadata.columns)
-    select_cat = st.selectbox('Choose subsetting category if applicable', categories)
+    if 'subset' not in st.session_state or  st.session_state['subset'] == 'None':
+        categories = sorted(metadata.columns)
+        select_cat = st.selectbox('Choose subsetting category if applicable', categories)
+        if select_cat:
+            choices = sorted(set(metadata[select_cat]))
+            select_choice = st.multiselect('Choose item from the category', choices)
+            if select_choice:
+                st.session_state['subset'] = {select_cat : select_choice}
     
-    choices = sorted(set(metadata[select_cat]))
-    select_choice = st.multiselect('Choose item from the category', choices)
-    
-    subset = {}
-    if select_choice:
-        subset = {select_cat : select_choice}
-
     launch = st.button("Let's go!")
     
     # this is where it really starts to run
@@ -66,20 +57,28 @@ if uploaded_mtx and uploaded_genes and uploaded_metadata:
         and (len(set(receptor_names) & set(gene_names_all))==len(receptor_names))
         and (len(ligand_names)==len(receptor_names)) 
         and launch):
+
+        gene_names = sorted(set(ligand_names) | set(receptor_names))
+        subunit_info = [f'{i} | {j}' for i,j in zip(ligand_names, receptor_names)]
+        gene_index = [gene_names_all.index(i) for i in gene_names]
         
-        work_text = st.text('Working....')
-      
+        data_load_state = st.text('Loading matrix data (this can take a minute)')
+        scRNA_array = ds.load_mtx_data(uploaded_mtx, 
+                                   metadata,
+                                   gene_index)
+        
+        data_load_state.text('Done!')
+       
+        if 'subset' not in st.session_state:
+            subset = {}
+        else:
+            subset = st.session_state['subset']
         
         scRNA_array, cell_type_labels = ds.filter_out(scRNA_array,
                                                    metadata,
                                                    select_track,
                                                    EXCLUDE_ITEMS,
                                                    subset)
-        
-        gene_names = sorted(set(ligand_names) | set(receptor_names))
-        subunit_info = [f'{i} | {j}' for i,j in zip(ligand_names, receptor_names)]
-        gene_index = [gene_names_all.index(i) for i in gene_names]
-        scRNA_array = scRNA_array[gene_index, : ]
 
         
         average_expression_df, number_expression_df, totalcell_df, cell_type_specific_expressions_df, unique_cell_type_labels, cell_type_numbers = ds.calculate_celltype_average_expressions(scRNA_array,
@@ -128,20 +127,20 @@ if uploaded_mtx and uploaded_genes and uploaded_metadata:
                           number_expression_df,
                           totalcell_df)
         
-        work_text.text('Done working!')
-        del st.session_state['metadata']
-        del st.session_state['scRNA']
+        st.session_state['subset'] = 'None'
+
         
     elif len( (set(ligand_names) | set(receptor_names)) - set(gene_names_all) ) >= 1:
         not_in_dataset = (set(ligand_names) | set(receptor_names)) - set(gene_names_all)
         if not_in_dataset and not_in_dataset != set(['']):
-            col1.text(f'{not_in_dataset} not present in this dataset!')        
+            col1.text(f'{not_in_dataset} not present in this dataset!')
                 
     elif ligand_names and receptor_names and launch:
         col1.text('Please make sure that the number of ligands and receptors is the same')
         col1.text(f'The ligands are {ligand_names}')
         col1.text(f'The corresponding receptors are {receptor_names}')
- 
+
+    
 elif not uploaded_mtx or not uploaded_genes or not uploaded_metadata:
     data_load_state = st.text('Please load the matrix, the genes, and the metadata (csv or json)')
 
